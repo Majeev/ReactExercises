d24a28e93d3d284787876af18cb67211f5ba02b5
Jakub Majewski <majewski.j01@gmail.com>
2022-05-27 20:36:45 +0200

    hello world!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ItemList from './components/ItemList/ItemList'
+
+
+function App() {
+  return (
+    <div id="app">
+      <ItemList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import css from './ItemList.css'
+
+const ItemList = () => {
+    return(
+        <div className={css.ItemList}>Hello</div>
+    )
+}
+export default ItemList;
