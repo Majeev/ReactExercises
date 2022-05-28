8792bc00f7ad38c5420468632faa41f3a7ca463b
Jakub Majewski <majewski.j01@gmail.com>
2022-05-28 13:35:05 +0200

    updated basic css

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,8 +2,62 @@ import React from "react";
 import css from './ItemList.css'
 
 const ItemList = () => {
-    return(
-        <div className={css.ItemList}>Hello</div>
+    return (
+        <div className='page'>
+            <div className="container">
+                <header>
+                    <h1>To Do List</h1>
+                </header>
+
+                <form>
+                    <input type='text' placeholder='add your todo' />
+                    <button type='submit'>Add</button>
+                </form>
+
+                <ul className='item-list'>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                    <div className="item-wrapper">
+                        <li>Lorem Ipsum</li>
+                        <button>Remove</button>
+                    </div>
+                </ul>
+            </div>
+        </div>
     )
 }
 export default ItemList;
